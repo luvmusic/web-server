b60ab782cf20c9762b3508a1d50d1bbbccd9f6dd
Julian Macri <luvmusic@alum.mit.edu>
2016-10-23 10:38:35 -0400

    commit via git gui

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-module.exports =
+middleware =
 {
 	authenticator: function(request, response, next)
 	{
@@ -8,9 +8,11 @@ module.exports =
 	logger: function(request, response, next)
 	{
 		var theRequest = request.method + " " + request.originalUrl
-		
+
 		console.log("Enter(" + new Date().toString() + ") " + theRequest);
 		next();
 		console.log("Exit(" + new Date().toString() + ") " + theRequest);
 	}
 };
+
+module.exports = middleware;
